0818d90e7a5f605f02167b570e4478e5407be955
Eric Vinicius Felix Silva <ericviniciusfelixsilva@gmail.com>
2024-09-25 18:55:51 -0400

    add GameManager and Singleton
    main changes: GameManager and Singleton files created

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header("Player")]
+    public GameObject playerPrefab;
+
+    [Header("Enemies")]
+    public List<GameObject> enemies;
+
+    [Header("Referencies")]
+    public Transform startPoint;
+    private GameObject _currentPlayer;
+
+     [Header("Animation")]
+    public float duration = .5f;
+    public float delay =.1f;
+    public Ease ease = Ease.OutBack;
+
+    private void Start() {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From().SetDelay(delay);
+    }
+
+}
